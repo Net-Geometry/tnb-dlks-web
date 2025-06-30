04fec8d49fc00e369758e7257e908041bf899af9
Mustaqeem <blur855@gmail.com>
2025-06-30 19:25:35 +0800

    fix: revert background gradient to solid colors in DashboardLayout

--- src/component/layout/DashboardLayout.tsx
+++ src/component/layout/DashboardLayout.tsx
@@ -8,7 +8,7 @@ const DashboardLayout = ({ children}:DashboardLayoutProps) => {
   return (
     <div>
         DashboardLayout
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+        <div className="min-h-screen bg-white dark:bg-gray-900">
             {children}
         </div>
     </div>
